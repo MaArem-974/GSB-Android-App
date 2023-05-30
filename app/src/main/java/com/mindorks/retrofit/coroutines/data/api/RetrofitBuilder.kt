2445ee5af9f59876b30824b2d75272229fe3c84e
Keryan.M <keryanmercadier974@gmail.com>
2023-05-30 08:52:34 +0400

    Appli Retrofit pour Hyper-V

--- app/src/main/java/com/mindorks/retrofit/coroutines/data/api/RetrofitBuilder.kt
+++ app/src/main/java/com/mindorks/retrofit/coroutines/data/api/RetrofitBuilder.kt
@@ -1,17 +1,29 @@
 package com.mindorks.retrofit.coroutines.data.api
 
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
+
 object RetrofitBuilder {
 
-    private const val BASE_URL = "http://10.0.2.2"
+// Ancien Ip 10.0.2.2
+    private const val BASE_URL = "http://10.0.8.16"
+
+    private fun Gson(): Gson{
+        return GsonBuilder()
+            .setLenient()
+            .create()
+
+    }
 
     private fun getRetrofit(): Retrofit {
         return Retrofit.Builder()
             .baseUrl(BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
+            .addConverterFactory(GsonConverterFactory.create(Gson()))
             .build()
+
     }
 
     val apiService: ApiService = getRetrofit().create(ApiService::class.java)
